d79fff62876b1d9c044c0ef0a4e3888a50535684
Sumeet PATIL <sumeet.patil@sap.com>
2022-04-27 11:59:14 +0200

    Test

--- src/main/java/com/student/App.java
+++ src/main/java/com/student/App.java
@@ -1,3 +1,5 @@
+package com.student;
+
 import java.sql.*;  
 class App{  
 	public static void main(String args[]){  
@@ -6,8 +8,8 @@ class App{
 
 	public static void selectData(){
 		try{  
-			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/sonoo","root","root");   
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			Connection con = DriverManager.getConnection("jdbc:mysql://192.100.0.000:3306/DBname", "root", "root");  
 			String category = System.getenv("ITEM_CATEGORY");
 			Statement statement = con.createStatement();
 			String query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
