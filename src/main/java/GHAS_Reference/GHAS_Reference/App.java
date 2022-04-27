efc32c5cfead2998926371f9230069285520dc3a
sumeet patil <sumeet.patil@sap.com>
2022-04-27 11:40:51 +0200

    Update App.java

--- src/main/java/GHAS_Reference/GHAS_Reference/App.java
+++ src/main/java/GHAS_Reference/GHAS_Reference/App.java
@@ -1,18 +1,18 @@
 import java.sql.*;  
 class App{  
 	public static void main(String args[]){  
-		selectData("test");
+		selectData();
 	}  
 
-	public static void selectData(String emp){
+	public static void selectData(){
 		try{  
 			Class.forName("com.mysql.jdbc.Driver");  
 			Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/sonoo","root","root");   
-			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery("select * from emp where empId="+emp);  
-			while(rs.next())  
-				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-				con.close();  
+			String category = System.getenv("ITEM_CATEGORY");
+			Statement statement = con.createStatement();
+			String query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
+				+ category + "' ORDER BY PRICE";
+			    ResultSet results = statement.executeQuery(query1); 
 		}catch(Exception e){ 
 			System.out.println(e);
 		}  
